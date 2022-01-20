1b7ea8a5083e4de0873081b843afd3aceaf54517
Shikha4633 <97088156+Shikha4633@users.noreply.github.com>
2022-01-20 20:54:25 +0530

    Add files via upload

---
+++ ECE_21105062_Deep Shikha_Assignment 2/21105062_Output_Assignment2_ITC.py
@@ -0,0 +1,97 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+====================================================== RESTART: C:/Users/Deep Shikha/OneDrive/Desktop/21105062_Assignment2_ITC.py ======================================================
+Ques.1}
+
+
+Type Your String: Python is a case sensitive language
+
+a. Length of the entered string is:  35 
+
+b. Reversed Order of String:  egaugnal evitisnes esac a si nohtyP 
+
+c. New String is:  a case sensitive 
+
+d. Repaced String is :  Python is object oriented language 
+
+e. Index of Substring 'a' is :  10 
+
+Your String with no spaces Pythonisacasesensitivelanguage
+
+
+
+
+
+Ques.2}
+
+
+Enter Your Name: Deep Shikha
+Enter YOur SID: 21105062
+Enter Your Department: ECE
+Enter Your CGPA: 8.9
+Hey, Deep Shikha Here!
+My SID is 21105062
+I am from ECE Departmentand My CGPA is 8.900000
+
+
+
+
+
+Ques.3}
+
+
+a=  56 
+b=  10
+a. a&b=  8 
+
+b. a|b=  58 
+
+c. a^b=  50 
+
+d. Left Shift of 'a' by 2 bits=  224 
+Left Shift od 'b' by 2 bits=  40
+e. Right Shift of 'a' by 2 bits=  14 
+Right Shift od 'b' by 4 bits=  0
+
+
+
+
+
+Ques.4}
+
+
+Enter three numbers to find the greatest number:- 
+First Number= 56
+Second Number= 66
+Third Number= 78
+
+Greatest Number is:  78.0
+
+
+
+
+
+Ques.5}
+
+
+Enter a sentence to know wheteher it contains 'name' or not:-
+
+Enter your sentence: Hey my name is Deep Shikha
+
+ Does this sentence contain word 'name' :  Yes
+
+
+
+
+
+Ques.6}
+
+
+Enter three lengths to see whether a triangle can be formed from these lengths or not:-
+
+Length of Frist Side of Triangle= 56
+Length of Second Side of Triangle= 77
+Length of Third Side of Triangle= 67
+
+Yes,Triangle can be formed.
