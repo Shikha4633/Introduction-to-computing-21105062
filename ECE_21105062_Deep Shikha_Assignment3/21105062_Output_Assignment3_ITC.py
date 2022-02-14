94536dbb868c447c4fb883e8c6dc22a99bb77445
Shikha4633 <97088156+Shikha4633@users.noreply.github.com>
2022-02-14 23:35:08 +0530

    Add files via upload

---
+++ ECE_21105062_Deep Shikha_Assignment3/21105062_Output_Assignment3_ITC.py
@@ -0,0 +1,103 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:/Users/Deep Shikha/OneDrive/Desktop/Deep Shikha/21105062_Assignment3_ITC.py
+The solution of Question 1 is:
+
+Enter the string: Shikha tells shikha that her name is shikha so shikha agrees that her name is Shikha
+The occurence(s) of:
+	"[1mshikha[0m" is/are [1m5[0m
+	"[1mtells[0m" is/are [1m1[0m
+	"[1mthat[0m" is/are [1m2[0m
+	"[1mher[0m" is/are [1m2[0m
+	"[1mname[0m" is/are [1m2[0m
+	"[1mis[0m" is/are [1m2[0m
+	"[1mso[0m" is/are [1m1[0m
+	"[1magrees[0m" is/are [1m1[0m
+
+The solution of Question 2 is:
+
+Date - 31
+Month - 2
+Year - 2020
+The given month has only 29 days
+Please enter a valid date
+Date - 27
+Month - 7
+Year - 2021
+Next Date is: 28/7/2021
+
+The solution of Question 3 is:
+
+Enter the list of integers: [2,3,4,5,6,7]
+Output: [(2, 4), (3, 9), (4, 16), (5, 25), (6, 36), (7, 49)]
+
+The solution of Question 4 is:
+
+Enter the grade point of the student: 9.7
+Your Grade is 'A' and Excellent Performance
+
+The solution of Question 5 is:
+
+ABCDEFGHIJK
+ ABCDEFGHI
+  ABCDEFG
+   ABCDE
+    ABC
+     A
+
+The solution of Question 6 is:
+
+Enter the name of the student: Deep Shikha
+Enter the SID of Deep Shikha: 21105062
+
+You have entered 1 value(s) till now
+Do you want to enter more data? y
+Enter the name of the student: Darsh
+Enter the SID of Darsh: 21105064
+
+You have entered 2 value(s) till now
+Do you want to enter more data? y
+Enter the name of the student: Vishesh
+Enter the SID of Vishesh: 21105056
+
+You have entered 3 value(s) till now
+Do you want to enter more data? n
+
+a. Student Details:
+The SID of [1mDeep Shikha[0m is [1m21105062[0m
+The SID of [1mDarsh[0m is [1m21105064[0m
+The SID of [1mVishesh[0m is [1m21105056[0m
+
+b. Student Details (sorted with respect to names):
+The SID of [1mDarsh[0m is [1m21105064[0m
+The SID of [1mDeep Shikha[0m is [1m21105062[0m
+The SID of [1mVishesh[0m is [1m21105056[0m
+
+c. Student Details (sorted with respect to SIDs):
+The SID of [1mVishesh[0m is [1m21105056[0m
+The SID of [1mDeep Shikha[0m is [1m21105062[0m
+The SID of [1mDarsh[0m is [1m21105064[0m
+
+d. Enter the SID of the student: 243
+The SID you entered isn't entered
+Please enter a valid SID to be searched
+List of valid SIDs: [21105062, 21105064, 21105056]
+
+Enter the SID of the student: 21105062
+The name of student whose SID is 21105062 is [1mDeep Shikha[0m
+
+The solution of Question 7 is:
+
+Enter the no. of terms of the Fibonacci sequence: 8
+The Fibonacci sequence is as follows:
+0 1 1 2 3 5 8 13 
+The average of the terms of Fibonacci sequence upto 8 terms is 4.12
+
+The solution of Question 8 is:
+
+a. Set of all elements that are in Set1 and Set2 but not both: {1, 3, 5, 6, 8}
+b. Set of all elements that are in only one of the three sets Set1, Set2 and Set3: {17, 3, 6, 8, 9, 13}
+c. Set of elements that are in exactly two of the sets Set1, Set2 and Set3: {1, 2, 4, 5}
+d. Set of all integers in the range 1 to 10 that are not in Set1: {6, 7, 8, 9, 10}
+e. Set of all integers in the range 1 to 10 that are not in Set1, Set2 and Set3: {10, 7}
